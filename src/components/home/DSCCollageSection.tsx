d08cf7f6557fe60f6757b3fe488f95ddaa3da387
Sergio Marentes <sergiomarentes705@gmail.com>
2026-05-05 21:14:03 -0500

    corrections on grid

--- src/components/home/DSCCollageSection.tsx
+++ src/components/home/DSCCollageSection.tsx
@@ -4,9 +4,7 @@ import "./DSCCollageSection.css";
 // Importar las imágenes DSC con nombres limpios
 import dsc2 from "../../assets/DSC01457.png";
 import dsc1 from "../../assets/DSC01432.png";
-import dsc6 from "../../assets/DSC01441.png";
 import dsc7 from "../../assets/DSC01487.png";
-import dsc5 from "../../assets/DSC01492.png";
 import dsc3 from "../../assets/DSC01380.png";
 import dsc4 from "../../assets/DSC01402.png";
 
@@ -14,10 +12,8 @@ function DSCCollageSection() {
   const images = [
     { src: dsc3, alt: "Imagen profesional 3", className: "collage-img-main" },
     { src: dsc2, alt: "Imagen profesional 2", className: "collage-img-small" },
-    { src: dsc5, alt: "Imagen profesional 5", className: "collage-img-small" },
     { src: dsc4, alt: "Imagen profesional 4", className: "collage-img-small" },
     { src: dsc1, alt: "Imagen profesional 1", className: "collage-img-small" },
-    { src: dsc6, alt: "Imagen profesional 6", className: "collage-img-small" },
     { src: dsc7, alt: "Imagen profesional 7", className: "collage-img-small" },
   ];
 
