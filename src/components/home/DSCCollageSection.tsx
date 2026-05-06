d2cc7e346a57aa10fed60618812677e0de14dbf5
Sergio Marentes <sergiomarentes705@gmail.com>
2026-05-05 20:29:03 -0500

    new images and corrections

--- src/components/home/DSCCollageSection.tsx
+++ src/components/home/DSCCollageSection.tsx
@@ -2,11 +2,13 @@ import { Box, Heading, Container } from "@chakra-ui/react";
 import "./DSCCollageSection.css";
 
 // Importar las imágenes DSC con nombres limpios
-import dsc1 from "../../assets/DSC01357.png";
-import dsc2 from "../../assets/DSC01432.png";
-import dsc3 from "../../assets/DSC01441.png";
-import dsc4 from "../../assets/DSC01487.png";
+import dsc2 from "../../assets/DSC01457.png";
+import dsc1 from "../../assets/DSC01432.png";
+import dsc6 from "../../assets/DSC01441.png";
+import dsc7 from "../../assets/DSC01487.png";
 import dsc5 from "../../assets/DSC01492.png";
+import dsc3 from "../../assets/DSC01380.png";
+import dsc4 from "../../assets/DSC01402.png";
 
 function DSCCollageSection() {
   const images = [
@@ -15,6 +17,8 @@ function DSCCollageSection() {
     { src: dsc5, alt: "Imagen profesional 5", className: "collage-img-small" },
     { src: dsc4, alt: "Imagen profesional 4", className: "collage-img-small" },
     { src: dsc1, alt: "Imagen profesional 1", className: "collage-img-small" },
+    { src: dsc6, alt: "Imagen profesional 6", className: "collage-img-small" },
+    { src: dsc7, alt: "Imagen profesional 7", className: "collage-img-small" },
   ];
 
   return (
